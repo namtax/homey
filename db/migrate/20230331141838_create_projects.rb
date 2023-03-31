069d9153f0db7d1b724c5a3060e49f11dd5b5872
ike <namtax@ikes-MBP.fritz.box>
2023-03-31 15:14:24 +0100

    Add models

---
+++ db/migrate/20230331141838_create_projects.rb
@@ -0,0 +1,10 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.integer :status, default: 0 
+
+      t.timestamps
+    end
+  end
+end
